190bf94ae7332df329518fc13f9dba82797c2acd
사라 <prinsaraouo@gmail.com>
2026-04-21 15:33:46 +0900

    first

---
+++ src/test/java/com/example/schedule_project2/ScheduleProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule_project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
